9467a5eccd43197967a14b2dd22d31685688aa6e
Heinrich Kruger <heindsight@kruger.dev>
2020-04-13 13:59:36 +0100

    Further test fixes

--- test/test_config.py
+++ test/test_config.py
@@ -8,87 +8,72 @@ from dhcp_notify import config as dhcp_notify_config
 from . import prune_config
 
 
-class TestSMTPHostConfig:
-    @pytest.mark.parametrize(
-        "smtp_host", ["smtp.example1.test", "smtp.example2.test"], scope="class",
-    )
-    def test_smtp_host(self, smtp_host, loaded_config):
-        assert loaded_config.smtp.host == smtp_host
+@pytest.mark.parametrize(
+    "smtp_host", ["smtp.example1.test", "smtp.example2.test"], scope="class",
+)
+def test_smtp_host(smtp_host, loaded_config):
+    assert loaded_config.smtp.host == smtp_host
 
 
-class TestSMTPPortConfig:
-    @pytest.mark.parametrize("smtp_port", ["587", "465", "25"], scope="class")
-    def test_smtp_port(self, smtp_port, loaded_config):
-        assert loaded_config.smtp.port == smtp_port
+@pytest.mark.parametrize("smtp_port", ["587", "465", "25"], scope="class")
+def test_smtp_port(smtp_port, loaded_config):
+    assert loaded_config.smtp.port == smtp_port
 
 
-class TestSMTPTLSConfig:
-    @pytest.mark.parametrize(
-        "smtp_tls_setting", ["tls", "starttls", "off"], scope="class",
-    )
-    def test_smtp_tls(self, smtp_tls_setting, loaded_config):
-        assert loaded_config.smtp.tls.value == smtp_tls_setting
+@pytest.mark.parametrize("smtp_tls_setting", ["tls", "starttls", "off"], scope="class")
+def test_smtp_tls(smtp_tls_setting, loaded_config):
+    assert loaded_config.smtp.tls.value == smtp_tls_setting
 
 
-class TestSMTPUserConfig:
-    @pytest.mark.parametrize(
-        "smtp_user", ["user@example.com", "user-test"], scope="class",
-    )
-    def test_smtp_username(self, smtp_user, loaded_config):
-        assert loaded_config.smtp.credentials.username == smtp_user
+@pytest.mark.parametrize("smtp_user", ["user@example.com", "user-test"], scope="class")
+def test_smtp_username(smtp_user, loaded_config):
+    assert loaded_config.smtp.credentials.username == smtp_user
 
 
-class TestSMTPPasswordConfig:
-    @pytest.mark.parametrize(
-        "smtp_password", ["53cr37%", "secret"], scope="class",
-    )
-    def test_smtp_password(self, smtp_password, loaded_config):
-        assert loaded_config.smtp.credentials.password == smtp_password
+@pytest.mark.parametrize("smtp_password", ["53cr37%", "secret"], scope="class")
+def test_smtp_password(smtp_password, loaded_config):
+    assert loaded_config.smtp.credentials.password == smtp_password
 
 
-class TestMessageFromAddrConfig:
-    @pytest.mark.parametrize(
-        "from_addr",
-        ["test-from@example.test", "DHCP Test <test-from@example.test>"],
-        scope="class",
-    )
-    def test_message_from_addr(self, from_addr, loaded_config):
-        assert loaded_config.message.from_addr == from_addr
+@pytest.mark.parametrize(
+    "from_addr",
+    ["test-from@example.test", "DHCP Test <test-from@example.test>"],
+    scope="class",
+)
+def test_message_from_addr(from_addr, loaded_config):
+    assert loaded_config.message.from_addr == from_addr
 
 
-class TestMessageToAddrConfig:
-    @pytest.mark.parametrize(
-        "to_addr",
-        ["test-to@example.test", "DHCP Test <test-to@example.test>"],
-        scope="class",
-    )
-    def test_message_to_addr(self, to_addr, loaded_config):
-        assert loaded_config.message.to_addr == to_addr
+@pytest.mark.parametrize(
+    "to_addr",
+    ["test-to@example.test", "DHCP Test <test-to@example.test>"],
+    scope="class",
+)
+def test_message_to_addr(to_addr, loaded_config):
+    assert loaded_config.message.to_addr == to_addr
 
 
-class TestMessageSubjectConfig:
-    @pytest.mark.parametrize(
-        "subject", ["Testing", "Test 123", "Thıß is a teṣt"], scope="class",
-    )
-    def test_message_subject(self, subject, loaded_config):
-        assert loaded_config.message.subject == subject
+@pytest.mark.parametrize(
+    "subject", ["Testing", "Test 123", "Thıß is a teṣt"], scope="class",
+)
+def test_message_subject(subject, loaded_config):
+    assert loaded_config.message.subject == subject
 
 
-class TestIgnoreMACsConfig:
-    @pytest.mark.parametrize(
-        "ignore_macs, expected_macs",
-        [
-            ([], []),
-            (["28:84:1E:e7:29:91"], ["28:84:1e:e7:29:91"]),
-            (
-                ["67:ad:98:cd:56:71", "C2:02:EA:34:23:D0"],
-                ["67:ad:98:cd:56:71", "c2:02:ea:34:23:d0"],
-            ),
-        ],
-        scope="class",
-    )
-    def test_ignore_macs(self, ignore_macs, expected_macs, loaded_config):
-        assert sorted(loaded_config.ignore_macs) == sorted(expected_macs)
+@pytest.mark.parametrize(
+    "ignore_macs, expected_macs",
+    [
+        ([], []),
+        (["28:84:1E:e7:29:91"], ["28:84:1e:e7:29:91"]),
+        (
+            ["67:ad:98:cd:56:71", "C2:02:EA:34:23:D0"],
+            ["67:ad:98:cd:56:71", "c2:02:ea:34:23:d0"],
+        ),
+    ],
+    scope="class",
+)
+def test_ignore_macs(ignore_macs, expected_macs, loaded_config):
+    assert sorted(loaded_config.ignore_macs) == sorted(expected_macs)
 
 
 class TestOptionalConfig:
@@ -159,50 +144,74 @@ class TestIgnoreExtraConfig:
 
 class RequiredConfigTestBase:
     @pytest.mark.usefixtures("config_file")
-    def test_raises_for_missing(self, config_path):
+    def test_raises_for_missing(self, config_path, cfg_class, missing):
         with pytest.raises(dhcp_notify_config.ConfigError) as e:
             dhcp_notify_config.load(str(config_path))
 
-        missing = ", ".join(self.required)
-        assert e.match(
-            f"{self.cfg_class}: missing values for required fields: {missing}"
-        )
+        missing = ", ".join(missing)
+        assert e.match(f"{cfg_class}: missing values for required fields: {missing}")
 
 
 class TestSMTPCredentialsConfigRequired(RequiredConfigTestBase):
-    required = ["username", "password"]
-    cfg_class = "Credentials"
+    @pytest.fixture(scope="class")
+    def cfg_class(self):
+        return "Credentials"
+
+    @pytest.fixture(
+        params=[["username"], ["password"], ["username", "password"]], scope="class",
+    )
+    def missing(self, request):
+        return request.param
 
     @pytest.fixture(scope="class")
-    def smtp_credentials(self, smtp_credentials):
-        return prune_config(smtp_credentials, *self.required)
+    def smtp_credentials(self, smtp_credentials, missing):
+        return prune_config(smtp_credentials, *missing)
 
 
 class TestSMTPConfigRequired(RequiredConfigTestBase):
-    required = ["host"]
-    cfg_class = "SMTPConfig"
+    @pytest.fixture(scope="class")
+    def cfg_class(self):
+        return "SMTPConfig"
 
     @pytest.fixture(scope="class")
-    def smtp_config(self, smtp_config):
-        return prune_config(smtp_config, *self.required)
+    def missing(self):
+        return ["host"]
+
+    @pytest.fixture(scope="class")
+    def smtp_config(self, smtp_config, missing):
+        return prune_config(smtp_config, *missing)
 
 
 class TestMessageConfigRequired(RequiredConfigTestBase):
-    required = ["from_addr", "to_addr"]
-    cfg_class = "MessageConfig"
+    @pytest.fixture(scope="class")
+    def cfg_class(self):
+        return "MessageConfig"
+
+    @pytest.fixture(
+        params=[["from_addr"], ["to_addr"], ["from_addr", "to_addr"]], scope="class",
+    )
+    def missing(self, request):
+        return request.param
 
     @pytest.fixture(scope="class")
-    def message_config(self, message_config):
-        return prune_config(message_config, *self.required)
+    def message_config(self, message_config, missing):
+        return prune_config(message_config, *missing)
 
 
 class TestTopLevelConfigRequired(RequiredConfigTestBase):
-    required = ["smtp", "message"]
-    cfg_class = "Config"
+    @pytest.fixture
+    def cfg_class(self):
+        return "Config"
+
+    @pytest.fixture(
+        params=[["smtp"], ["message"], ["smtp", "message"]], scope="class",
+    )
+    def missing(self, request):
+        return request.param
 
     @pytest.fixture(scope="class")
-    def config(self, config):
-        return prune_config(config, *self.required)
+    def config(self, config, missing):
+        return prune_config(config, *missing)
 
 
 class TestInvalidConfigFile:
